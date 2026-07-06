aa91279a635f3fec864341019f095325d77b23a1
Danihmn <daniel.bezerra.mult@gmail.com>
2026-07-05 22:46:45 -0300

    Codifica Claims que terão no Token

--- Store.Infrastructure/Security/Services/TokenService.cs
+++ Store.Infrastructure/Security/Services/TokenService.cs
@@ -17,15 +17,19 @@ public class TokenService
         {
             SigningCredentials = credentials,
             Expires = DateTime.UtcNow.AddHours(2),
-            Subject = new ClaimsIdentity(
-            [
-                new Claim("sub", user.Id.ToString()),
-                new Claim("email", user.Email.Value),
-                new Claim("role", user.Role.Value)
-            ])
+            Subject = GenerateClaims(user)
         };
         var token = handler.CreateToken(tokenDescriptor);
 
         return handler.WriteToken(token);
     }
+
+    private static ClaimsIdentity GenerateClaims (User user)
+        => new(
+            [
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email.Value),
+                new Claim(JwtRegisteredClaimNames.Name, user.Name),
+                new Claim("role", user.Role.Value)
+            ]);
 }
